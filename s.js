9a7913aece7c7014d2cb75cc6784b3ebdde4e126
Arun surendharan <64095520+arun-me@users.noreply.github.com>
2020-05-07 13:31:06 +0530

    Add files via upload
    bg added

--- s.js
+++ s.js
@@ -29,18 +29,41 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
 // gradient
-var css = document.querySelector("h3");
+
+var css1 = document.querySelector(".h31");
+var css2 = document.querySelector(".h32");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var color4 = document.querySelector(".color4");
 var body = document.getElementById("gradient");
-
+var test = document.getElementById("test");
 function setGradient() {
+	css1.textContent="";
+	body.style.backgroundColor="";
 	body.style.background = 
 	"linear-gradient(to right,"+ color1.value + ", "+ color2.value + ")";
-	css.textContent = body.style.background + ";";
+	css1.textContent = body.style.background + ";";
 }
 
+function setBackground() {
+	css1.textContent="";
+	body.style.background="";
+	body.style.backgroundColor = color3.value;
+	css1.textContent = "backgroundColor = "+ body.style.backgroundColor + ";";
+}
+function setFontColor() {
+	test.style.color = color4.value;
+	css2.textContent = test.style.color ;
+}
+// document.getElementById("gradient").style.display="none";
+document.getElementById("p1").innerHTML=Date();
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setBackground);
+
+color4.addEventListener("input", setFontColor);
